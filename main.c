ab776bddf9ee506c006dd9736ce087dc519d473b
Jim <dimitriss.theodoropoulos@gmail.com>
2021-01-26 18:09:44 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+void ascithema() {
+
+    int N;
+    char **pin;
+    int i;
+    printf("Dwse mou enan arithmo TWRA: ");
+    scanf("%d", &N);
+    pin = (char **) malloc(N*sizeof(char*));
+    for(i = 0; i < N; i++) {
+        pin[i] = (char *) calloc(N, 1);
+        if (pin == NULL) {
+            printf("Δεν υπάρχει αρκετός χώρος!!");
+            exit(1);
+        }
+    }
+    int sum=0;
+    char randomletter;
+    for(i = 0; i < N; i++) {
+        randomletter = 'A' + (random() % 26);
+        // OR randomletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[random () % 26];
+        printf("ASCII value of %c = %d\n", randomletter, randomletter);
+        sum += randomletter;
+        *pin[i] = randomletter;
+
+    }
+    for(i = 0; i < N; i++) {
+        printf("%s ", pin[i]);
+    }
+    printf("\nsum == %d\n", (sum/N));
+}
+int main(){
+
+  srand(time(NULL));
+  ascithema();
+
+}
